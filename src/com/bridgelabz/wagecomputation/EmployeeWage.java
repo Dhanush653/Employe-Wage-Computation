692056456f995d9f2235deea50adc9ad4804fd61
Dhanush Kumar <dk652003@gmail.com>
2024-01-24 13:24:50 +0530

    Added EmployeeWage

--- src/com/bridgelabz/wagecomputation/EmployeeWage.java
+++ src/com/bridgelabz/wagecomputation/EmployeeWage.java
@@ -4,7 +4,13 @@ public class EmployeeWage {
 
     public static void main(String[] args) {
         int full_time = 1;
+        int emp_rate_per_hour = 20;
+        int emphr = 0;
+        int empWage = 0;
         double empCheck = Math.floor(Math.random() *10) % 2;
+
+        int wage = emp_rate_per_hour * 8;
+        System.out.println(wage);
 
         if (empCheck == full_time)
             System.out.println("Present");
