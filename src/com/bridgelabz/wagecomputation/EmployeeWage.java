e205dfde138d36f0ae7f5e1bab6e69c9eccd4c9a
Dhanush Kumar <dk652003@gmail.com>
2024-01-24 13:33:28 +0530

    Added EmpWage Using Switch

--- src/com/bridgelabz/wagecomputation/EmployeeWage.java
+++ src/com/bridgelabz/wagecomputation/EmployeeWage.java
@@ -1,22 +1,25 @@
 package com.bridgelabz.wagecomputation;
 
 public class EmployeeWage {
+    public static final int part_time = 1;
+    public static final int full_time = 2;
+    public static final int emp_rate_per_hour = 20;
 
     public static void main(String[] args) {
-        int full_time = 1;
-        int part_time = 2;
-        int emp_rate_per_hour = 20;
 
         int emphr = 0;
         int empWage = 0;
-        double empCheck = Math.floor(Math.random() *10) % 2;
-
-        if (empCheck == part_time)
-            emphr = 4;
-        else if (empCheck == full_time)
-            emphr = 8;
-        else
-            emphr = 1;
+        int empCheck =(int) Math.floor(Math.random() *10) % 3;
+        switch (empCheck) {
+            case part_time:
+                emphr = 4;
+                break;
+            case full_time:
+                emphr = 8;
+                break;
+            default:
+                emphr = 1;
+        }
         empWage = emphr * emp_rate_per_hour;
         System.out.println("Emp Wage: " + empWage);
     }
