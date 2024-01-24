6f67525358c6bbb6ce3fb4ec1f71de609ff5faa0
Dhanush Kumar <dk652003@gmail.com>
2024-01-24 13:39:22 +0530

    Added Total Working Hours

--- src/com/bridgelabz/wagecomputation/EmployeeWage.java
+++ src/com/bridgelabz/wagecomputation/EmployeeWage.java
@@ -1,18 +1,13 @@
 package com.bridgelabz.wagecomputation;
 
-public class EmployeeWage {
-    public static final int part_time = 1;
-    public static final int full_time = 2;
-    public static final int emp_rate_per_hour = 20;
-    public static final int working_days = 2;
+public static void main(String[] args) {
 
-    public static void main(String[] args) {
-
-        int emphr = 0;
-        int empWage = 0;
-        totalEmpWage = 0;
-        for (int day =0; day< working_days;day++)
-            int empCheck =(int) Math.floor(Math.random() *10) % 3;
+    int emphr = 0;
+    int totalEmpWage = 0;
+    int totalWorkingDays = 0;
+    while ( emphr <= hours_in_month && totalWorkingDays < working_days) {
+        totalWorkingDays++;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
         switch (empCheck) {
             case part_time:
                 emphr = 4;
@@ -23,9 +18,10 @@ public class EmployeeWage {
             default:
                 emphr = 1;
         }
-        empWage = emphr * emp_rate_per_hour;
-        totalEmpWage += empWage;
-        System.out.println("Emp Wage: " + empWage);
+        totalEmpWage += emphr;
+        System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +emphr);
     }
+    totalEmpWage = emphr * emp_rate_per_hour;
     System.out.println("Total Emp Wage: " + totalEmpWage);
+}
 }
