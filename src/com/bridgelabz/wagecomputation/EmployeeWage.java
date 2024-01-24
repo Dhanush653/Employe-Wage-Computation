e83f635f359839d4be861a93884a3fb149f1fa2f
Dhanush Kumar <dk652003@gmail.com>
2024-01-24 13:36:18 +0530

    Added Wage For Month

--- src/com/bridgelabz/wagecomputation/EmployeeWage.java
+++ src/com/bridgelabz/wagecomputation/EmployeeWage.java
@@ -4,12 +4,15 @@ public class EmployeeWage {
     public static final int part_time = 1;
     public static final int full_time = 2;
     public static final int emp_rate_per_hour = 20;
+    public static final int working_days = 2;
 
     public static void main(String[] args) {
 
         int emphr = 0;
         int empWage = 0;
-        int empCheck =(int) Math.floor(Math.random() *10) % 3;
+        totalEmpWage = 0;
+        for (int day =0; day< working_days;day++)
+            int empCheck =(int) Math.floor(Math.random() *10) % 3;
         switch (empCheck) {
             case part_time:
                 emphr = 4;
@@ -21,6 +24,8 @@ public class EmployeeWage {
                 emphr = 1;
         }
         empWage = emphr * emp_rate_per_hour;
+        totalEmpWage += empWage;
         System.out.println("Emp Wage: " + empWage);
     }
+    System.out.println("Total Emp Wage: " + totalEmpWage);
 }
